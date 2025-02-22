3d1f996ef4053f5aff136b252df38f313a5c0ad4
BugHunter-X <askzenai@gmail.com>
2025-02-22 22:54:10 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,27 @@
+```javascript
+//tailwind.config.js
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+```
+```html
+<!--index.html-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="/dist/output.css" rel="stylesheet">
+</head>
+<body>
+    <div class="bg-gradient-to-r from-green-400 to-blue-500">
+        <h1>Hello</h1>
+    </div>
+</body>
+</html>
+```
